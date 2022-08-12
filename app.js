e2afaeff690eb3cefdcbf308014a84de756688fe
iDominate <68128371+iDominate@users.noreply.github.com>
2022-08-12 15:07:11 +0200

    Update app.js
    Fixed Several Bugs

--- app.js
+++ app.js
@@ -2,23 +2,47 @@
 
 
 $(function () {
+
     $('#btn-slide').on('click', function () {
+        if (!$('input[type="hidden"]').val()) return;
         $('#rating').html($('#hidden-input').val());
         $('.item-container').css({ display: 'none' });
         $('.second').css({ display: 'block' });
     })
     $('#1').on('click', function () {
         $(this).css('background-color', 'rgb(252, 118, 19)');
-        $('#1').parent().children().not(this).each(function () {
-            $(this).css('background-color', 'rgb(44, 44, 52)');
+        $(this).hover(function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        }, function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        })
+
+        $('.rate-container > *:not("#1")').each(function () {
+            $(this).css('background-color', 'rgb(44, 44, 52)')
+            $(this).hover(function () {
+                $(this).css('background-color', 'skyblue')
+            }, function () {
+                $(this).css('background-color', 'rgb(44, 44, 52)')
+            })
         });
+
         $('#hidden-input').val('1');
     })
 
     $('#2').on('click', function () {
         $(this).css('background-color', 'rgb(252, 118, 19)');
-        $('#2').parent().children().not(this).each(function () {
-            $(this).css('background-color', 'rgb(44, 44, 52)');
+        $(this).hover(function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        }, function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        })
+        $('.rate-container > *:not("#2")').each(function () {
+            $(this).css('background-color', 'rgb(44, 44, 52)')
+            $(this).hover(function () {
+                $(this).css('background-color', 'skyblue')
+            }, function () {
+                $(this).css('background-color', 'rgb(44, 44, 52)')
+            })
         });
         $('#hidden-input').val('2');
 
@@ -26,8 +50,19 @@ $(function () {
 
     $('#3').on('click', function () {
         $(this).css('background-color', 'rgb(252, 118, 19)');
-        $('#3').parent().children().not(this).each(function () {
-            $(this).css('background-color', 'rgb(44, 44, 52)');
+        $(this).hover(function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        }, function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        })
+        $('.rate-container > *:not("#3")').each(function () {
+            $(this).css('background-color', 'rgb(44, 44, 52)')
+            $(this).hover(function () {
+                $(this).css('background-color', 'skyblue')
+            }, function () {
+                $(this).css('background-color', 'rgb(44, 44, 52)')
+            })
+
         });
         $('#hidden-input').val('3');
 
@@ -35,8 +70,18 @@ $(function () {
 
     $('#4').on('click', function () {
         $(this).css('background-color', 'rgb(252, 118, 19)');
-        $('#4').parent().children().not(this).each(function () {
+        $(this).hover(function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        }, function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        })
+        $('.rate-container > *:not("#4")').each(function () {
             $(this).css('background-color', 'rgb(44, 44, 52)')
+            $(this).hover(function () {
+                $(this).css('background-color', 'skyblue')
+            }, function () {
+                $(this).css('background-color', 'rgb(44, 44, 52)')
+            })
         });
         $('#hidden-input').val('4')
 
@@ -44,8 +89,18 @@ $(function () {
 
     $('#5').on('click', function () {
         $(this).css('background-color', 'rgb(252, 118, 19)');
-        $('#5').parent().children().not(this).each(function () {
+        $(this).hover(function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        }, function () {
+            $(this).css('background-color', 'rgb(252, 118, 19)');
+        })
+        $('.rate-container > *:not("#5")').each(function () {
             $(this).css('background-color', 'rgb(44, 44, 52)')
+            $(this).hover(function () {
+                $(this).css('background-color', 'skyblue')
+            }, function () {
+                $(this).css('background-color', 'rgb(44, 44, 52)')
+            })
         });
         $('#hidden-input').val('5')
 
